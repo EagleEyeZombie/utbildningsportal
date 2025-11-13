0d95d80a267c699dcd43bb21532146a5b956458f
Fredrich Kjellberg <fredrich.kjellber@axxell.fi>
2025-11-13 11:38:49 +0200

    Fixat login-formulär för att tillåta användarnamn (text-fält)

--- login.php
+++ login.php
@@ -60,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login-submit'])) {
 
                         <div class="mb-3">
                             <label for="email" class="form-label">E-post eller Användarnamn</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
+                            <input type="text" class="form-control" id="email" name="email" required>
                         </div>
                         
                         <div class="mb-3">
