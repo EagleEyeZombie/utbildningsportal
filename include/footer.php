ccdf61b2477152330382a7f60f7c1a5b2f7d7402
Fredrich Kjellberg <fredrich.kjellber@axxell.fi>
2025-11-28 18:20:08 +0200

    fixat cookie information i footern.

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,11 @@
-
-
-
-
-
+<footer class="bg-dark text-white-60 mt-auto border-top border-secondary">
+    <div class="text-center">
+        <p class="m-1 py-1" style="font-size: 0.7rem; line-height: 1;">
+            &copy; <?php echo date("Y"); ?> KunskapsÄventyret. 
+            Endast nödvändiga cookies används.
+        </p>
+    </div>
+</footer>
 
 
 </body>
